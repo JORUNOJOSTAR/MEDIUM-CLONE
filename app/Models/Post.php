0422e70a96c43d356aaa3e8c6093c6a38f2571ca
JOJO <nwin919191@gmail.com>
2025-05-29 21:28:00 +0900

    add form for new post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'content',
+        'image',
+        'slug',
+        'user_id',
+        'category_id',
+        'published_at'
+    ];
 }
