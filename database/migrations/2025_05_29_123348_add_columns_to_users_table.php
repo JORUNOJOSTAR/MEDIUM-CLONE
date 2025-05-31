3ef82cfee3963e935fd5e7a3d7965e79cb20c861
JOJO <nwin919191@gmail.com>
2025-05-31 19:40:28 +0900

    update migration file to add column to user table

---
+++ database/migrations/2025_05_29_123348_add_columns_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    // username should be insert from user
+    // if username is defined using slug, there should be some way to make it unique
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('username')->unique()->after('id');
+            $table->string('image')->nullable()->after('email_verified_at');
+            $table->text('bio')->nullable()->after('image');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('username');
+            $table->dropColumn('image');
+            $table->dropColumn('bio');
+        });
+    }
+};
