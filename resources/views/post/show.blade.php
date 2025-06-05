963a68a285c2d7f21d15345d6ccbdfd4c9de6a3c
JOJO <nwin919191@gmail.com>
2025-06-05 20:32:06 +0900

    add show page for post content

---
+++ resources/views/post/show.blade.php
@@ -0,0 +1,55 @@
+<x-app-layout>
+    <div class="py-4">
+        <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-8">
+                <h1 class="text-5xl mb-4">{{ $post->title }}</h1>
+                {{-- User Avatar --}}
+                <div class="flex gap-4">
+                    @if ($post->user->image)
+                        <img src="{{ $post->user->imageUrl() }}" alt="{{ $post->user->name }}"
+                            class="w-12 h-12 rounded-full">
+                    @else
+                        <img src="https://pixy.org/src/31/315160.png" alt="Dummy avatar" class="w-12 h-12 rounded-full">
+                    @endif
+
+                    <div>
+                        <div class="flex gap-2">
+                            <h3>{{ $post->user->name }}</h3>
+                            <a href="#" class="text-emerald-500">Follow</a>
+                        </div>
+
+                        <div class="flex gap-2 text-sm text-gray-500">
+                            {{ $post->readTime() }} min read
+                            &middot;
+                            {{ $post->created_at->format('M d, Y') }}
+                        </div>
+                    </div>
+                </div>
+                {{-- User Avatar --}}
+
+                {{-- Clap Section --}}
+                <x-clap-button/>
+                {{-- Clap Section --}}
+
+                {{-- Content Section --}}
+                <div class="mt-8">
+                    <img src="{{ $post->imageUrl() }}" alt="{{ $post->title }}" class="w-full">
+                    <div class="mt-4">
+                        {{ $post->content }}
+                    </div>
+                </div>
+                {{-- Content Section --}}
+
+                <div class="mt-8">
+                    <span class="px-4 py-2 bg-gray-200 rounded-xl">
+                        {{ $post->category->name }}
+                    </span>
+                </div>
+
+                {{-- Clap Section --}}
+                <x-clap-button/>
+                {{-- Clap Section --}}
+            </div>
+        </div>
+    </div>
+</x-app-layout>
